d9b8ff8ce0a95f49feaef3975b6fb2134fa09a4f
coderinblack08 <kevinlu.email@gmail.com>
2023-10-29 19:54:39 -0700

    add modal for mcq and frq

--- components/quiz-forms.tsx
+++ components/quiz-forms.tsx
@@ -1,23 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import "katex/dist/katex.min.css";
 import React from "react";
 import { InlineMath } from "react-katex";
-import CourseLayout from "../../../../../components/course-layout";
-import { Button } from "../../../../../components/ui/button";
-import { useRouter } from "next/router";
-import {
-  CourseProgress,
-  SectionProgress,
-} from "../../../../../components/course-progress";
-import { BarChart } from "lucide-react";
-import {
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-} from "../../../../../components/ui/popover";
 
-interface CourseTestProps {}
-
-const MCQ: React.FC = () => {
+export const MCQ: React.FC = () => {
   return (
     <div className="max-w-xl mx-auto">
       <p>
@@ -58,36 +45,56 @@ const MCQ: React.FC = () => {
             <InlineMath math="e^{-x} x^{1+e}(-e+x)" />
           </span>
         </label>
-        <div className="flex gap-2 mt-4">
-          <Button className="w-full">Check</Button>
-          <Button className="w-full" variant="outline">
+        <label className="inline-flex items-center">
+          <input
+            type="radio"
+            className="form-radio text-primary-dark border-zinc-300"
+            name="radio"
+            value="3"
+          />
+          <span className="ml-2">None of the above</span>
+        </label>
+        <div className="space-y-2 mt-2">
+          <Button type="submit" name="check" className="w-full">
+            Check
+          </Button>
+          <Button
+            type="submit"
+            name="skip"
+            className="w-full"
+            variant="secondary"
+          >
             Skip
           </Button>
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button variant="outline">
-                <BarChart className="h-4 w-4 mr-2" />
-                Show stats
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent sideOffset={8}>
-              <CourseProgress />
-            </PopoverContent>
-          </Popover>
         </div>
       </form>
     </div>
   );
 };
 
-const CourseTest: React.FC<CourseTestProps> = () => {
-  const { query } = useRouter();
-
+export const FRQ: React.FC = () => {
   return (
-    <CourseLayout>
-      <div className="p-4 px-5 py-12"></div>
-    </CourseLayout>
+    <div className="max-w-xl mx-auto">
+      <p>
+        Given the function <InlineMath math="g(x)=\dfrac{e^x}{x^e}" />, find{" "}
+        <InlineMath math="\dfrac{d g(x)}{dx}" />.
+      </p>
+      <form>
+        <Input placeholder="Answer" className="mt-4" />
+        <div className="space-y-2 mt-2">
+          <Button type="submit" name="check" className="w-full">
+            Check
+          </Button>
+          <Button
+            type="submit"
+            name="skip"
+            className="w-full"
+            variant="secondary"
+          >
+            Skip
+          </Button>
+        </div>
+      </form>
+    </div>
   );
 };
-
-export default CourseTest;
